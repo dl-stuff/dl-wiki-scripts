cb80549433bc15610c09db68dcc62ee29db9a60b
mushymato <wildshinobu@gmail.com>
2019-09-29 09:49:44 -0400

    parse wyrmprint

--- Process_DL_Data.py
+++ Process_DL_Data.py
@@ -8,42 +8,123 @@ import argparse
 EXT = '.txt'
 DEFAULT_TEXT_LABEL = ''
 ENTRY_LINE_BREAK = '\n==============================\n'
+EDIT_THIS = '<EDIT_THIS>'
 
 ROW_INDEX = '_Id'
 
-TEXT_LABEL_FILE = 'TextLabel'
+TEXT_LABEL_NAME = 'TextLabel'
 TEXT_LABELS = None
+ABILITY_SHIFT_GROUP_NAME = 'AbilityShiftGroup'
+ABILITY_SHIFT_GROUPS = None
 
 ROMAN_NUMERALS = [None, 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
 ELEMENTS = [None, 'Flame', 'Water', 'Wind', 'Light', 'Shadow']
+AMULET_TYPE = [None, 'Attack', 'Defense', 'Support', 'Healing']
+
+def csv_as_index(path, index=None, tabs=False):
+    with open(path, newline='') as csvfile:
+        if tabs:
+            reader = csv.DictReader(csvfile, dialect='excel-tab')
+        else:
+            reader = csv.DictReader(csvfile)
+        first_row = next(reader)
+        key_iter = iter(first_row.keys())
+        csvfile.seek(0)
+        if not index:
+            index = next(key_iter) # get first key as index
+        if len(first_row) == 2:
+            # load 2 column files as dict[string] = string
+            value_key = next(key_iter) # get second key
+            return {row[index]: row[value_key] for row in reader if row[index] != '0'}
+        else:
+            # load >2 column files as a dict[string] = OrderedDict
+            return {row[index]: row for row in reader if row[index] != '0'}
 
 def get_label(key):
-    return TEXT_LABELS[key] if key in TEXT_LABELS else DEFAULT_TEXT_LABEL
+    return TEXT_LABELS[key].replace('\\n', ' ') if key in TEXT_LABELS else DEFAULT_TEXT_LABEL
 
-# All process_* functions take in 1 parameters (row) and return 3 parameters (new_row, template_name, display_name)
+# All process_* functions take in 1 parameter (OrderedDict row) and return 3 values (OrderedDict new_row, str template_name, str display_name)
+# Make sure the keys are added to the OrderedDict in the desired output order
+# It's alright to modify row instead of makeing new dict, but only do so when you want to print all the keys already present in row
 def process_AbilityLimitedGroup(row):
     row['_AbilityLimitedText'] = get_label(row['_AbilityLimitedText']).format(ability_limit0=row['_MaxLimitedValue'])
     return row, None, None
 
 def process_AbilityData(row):
     new_row = OrderedDict()
-    for k in ('PartyPowerWeight','AbilityIconName', 'AbilityLimitedGroupId1', 'AbilityLimitedGroupId2', 'AbilityLimitedGroupId3'):
-        new_row[k] = row['_' + k]
+
+    new_row['Id'] = row['_Id']
+    new_row['PartyPowerWeight'] = row['_PartyPowerWeight']
+    new_row['GenericName'] = '' # EDIT_THIS
+
+    shift_value = ROMAN_NUMERALS[int(row['_ShiftGroupId'])]
+    if row['_ShiftGroupId'] in ABILITY_SHIFT_GROUPS:
+        shift_group_row = ABILITY_SHIFT_GROUPS[row['_ShiftGroupId']]
+        for k, v in shift_group_row.items():
+            if k.startswith('_Level') and v == row['_Id']:
+                shift_value = ROMAN_NUMERALS[int(k.replace('_Level', ''))]
+    # TODO: figure out what actually goes here
+    ability_value = EDIT_THIS if row['_AbilityType1UpValue'] == '0' else row['_AbilityType1UpValue']
     new_row['Name'] = get_label(row['_Name']).format(
-        ability_shift0  =   ROMAN_NUMERALS[int(row['_ShiftGroupId'])],
-        ability_val0    =   row['_AbilityType1UpValue'])
-    new_row['Details'] = get_label(row['_Details']).format(
+        ability_shift0  =   shift_value,
+        ability_val0    =   ability_value)
+
+    # _ElementalType seems unreliable, use (element) in _Name for now
+    detail_label = get_label(row['_Details'])
+    if '{element_owner}' in detail_label and ')' in new_row['Name']:
+        element = new_row['Name'][1:new_row['Name'].index(')')]
+    else:
+        element = ELEMENTS[int(row['_ElementalType'])]
+    new_row['Details'] = detail_label.format(
         ability_cond0   =   row['_ConditionValue'],
-        ability_val0    =   row['_AbilityType1UpValue'],
-        element_owner   =   ELEMENTS[int(row['_ElementalType'])])
-    new_row['GenericName'] = ''
+        ability_val0    =   ability_value,
+        element_owner   =   element)
+
+    new_row['AbilityIconName'] = row['_AbilityIconName']
     new_row['AbilityGroup'] = row['_ViewAbilityGroupId1']
+    new_row['AbilityLimitedGroupId1'] = row['_AbilityLimitedGroupId1']
+    new_row['AbilityLimitedGroupId2'] = row['_AbilityLimitedGroupId2']
+    new_row['AbilityLimitedGroupId3'] = row['_AbilityLimitedGroupId3']
     return new_row, 'Ability', new_row['Name']
+
+def process_AmuletData(row):
+    new_row = OrderedDict()
+
+    new_row['Id'] = row['_Id']
+    new_row['BaseId'] = row['_BaseId']
+    new_row['Name'] = get_label(row['_Name'])
+    new_row['NameJP'] = '' # EDIT_THIS
+    new_row['FeaturedCharacters'] = '' # EDIT_THIS
+    new_row['Obtain'] = '' # EDIT_THIS
+    new_row['ReleaseDate'] = '' # EDIT_THIS
+    new_row['Availability'] = '' # EDIT_THIS
+    new_row['Rarity'] = row['_Rarity']
+    new_row['AmuletType'] = AMULET_TYPE[int(row['_AmuletType'])]
+    new_row['MinHp'] = row['_MinHp']
+    new_row['MaxHp'] = row['_MaxHp']
+    new_row['MinAtk'] = row['_MinAtk']
+    new_row['MaxAtk'] = row['_MaxAtk']
+    new_row['VariationId'] = row['_VariationId']
+    for i in range(1, 4):
+        for j in range(1, 4):
+            ab = 'Abilities{}{}'.format(i, j)
+            new_row[ab] = row['_' + ab]
+    new_row['Ability1Event'] = 0
+    new_row['Ability2Event'] = 0
+    new_row['Ability3Event'] = 0
+    new_row['ArtistCV'] = '' # EDIT_THIS
+    for i in range(1, 6):
+        new_row['FlavorText{}'.format(i)] = get_label(row['_Text{}'.format(i)])
+    new_row['IsPlayable'] = row['_IsPlayable']
+    new_row['SellCoin'] = row['_SellCoin']
+    new_row['SellDewPoint'] = row['_SellDewPoint']
+
+    return new_row, 'Wyrmprint', new_row['Name']
 
 DATA_FILE_PROCESSING = {
     'AbilityLimitedGroup': process_AbilityLimitedGroup,
     'AbilityData': process_AbilityData,
-    'AmuletData': None,
+    'AmuletData': process_AmuletData, # AKA Wyrmprint
     'BuildEventItem': None,
     'CharaData': None,
     'ExAbilityData': None,
@@ -70,25 +151,6 @@ DATA_FILE_PROCESSING = {
     'WeaponCraftTree': None,
 }
 
-def csv_as_index(path, index=None, tabs=False):
-    with open(path, newline='') as csvfile:
-        if tabs:
-            reader = csv.DictReader(csvfile, dialect='excel-tab')
-        else:
-            reader = csv.DictReader(csvfile)
-        first_row = next(reader)
-        key_iter = iter(first_row.keys())
-        csvfile.seek(0)
-        if not index:
-            index = next(key_iter) # get first key as index
-        if len(first_row) == 2:
-            # load 2 column files as dict[string] = string
-            value_key = next(key_iter) # get second key
-            return {row[index]: row[value_key] for row in reader if row[index] != '0'}
-        else:
-            # load >2 column files as a dict[string] = OrderedDict
-            return {row[index]: row for row in reader if row[index] != '0'}
-
 def build_wikitext_row(template_name, row, delim='|'):
     row_str = '{{' + template_name + delim
     row_str += delim.join(['{}={}'.format(k.strip('_'), row[k]) for k in row])
@@ -104,9 +166,8 @@ def csv_as_wikitext(in_dir, out_dir, data_name):
             template_name, display_name = None, None
             if DATA_FILE_PROCESSING[data_name] is not None:
                 row, template_name, display_name = DATA_FILE_PROCESSING[data_name](row)
-            if template_name is None:
-                template_name = data_name
-            if display_name:
+            template_name = data_name if template_name is None else template_name
+            if display_name is not None:
                 out_file.write(display_name)
                 out_file.write(ENTRY_LINE_BREAK)
                 out_file.write(build_wikitext_row(template_name, row, delim='|\n'))
@@ -154,7 +215,8 @@ if __name__ == '__main__':
     in_dir = args.i if args.i[-1] == '/' else args.i+'/'
     out_dir = args.o if args.o[-1] == '/' else args.o+'/'
 
-    TEXT_LABELS = csv_as_index(in_dir+TEXT_LABEL_FILE+EXT, tabs=True)
+    TEXT_LABELS = csv_as_index(in_dir+TEXT_LABEL_NAME+EXT, tabs=True)
+    ABILITY_SHIFT_GROUPS = csv_as_index(in_dir+ABILITY_SHIFT_GROUP_NAME+EXT)
 
     # find_fmt_params(in_dir, out_dir)
     for data_name in DATA_FILE_PROCESSING:
